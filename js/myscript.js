8c7fde02527791298fb00205f648f1d9eb9975c6
Ann Hayden <hayden.ann@gmail.com>
2018-04-26 10:07:44 +0000

    initial commit

---
+++ js/myscript.js
@@ -0,0 +1,8 @@
+//will assign css class .selection to the odd numbered table rows
+
+$(document).ready(function() {
+ $("tr").addClass("odd"); //underlines all <h2> elements
+  $("th").addClass("even"); //adds a border to all lists
+});
+
+
